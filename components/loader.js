9e806acb1d9346eb32156ddc4b7ab11d22700ffa
emam22 <emamendez6@gmail.com>
2026-06-03 20:50:13 -0300

    cambios responsive

--- components/loader.js
+++ components/loader.js
@@ -1,9 +1,10 @@
+const base = new URL('./', document.currentScript.src).href;
+
 async function loadComponent(elementId, filePath) {
-  const response = await fetch(filePath);
-  console.log(response);
+  const response = await fetch(base + filePath);
   const html = await response.text();
   document.getElementById(elementId).innerHTML = html;
 }
 
-loadComponent("navbar", "./navbar.html");
-loadComponent("footer", "./footer.html");
+loadComponent("navbar", "navbar.html");
+loadComponent("footer", "footer.html");
