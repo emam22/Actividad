955d90bb417c79269c98072f671a8881d4a0256f
emam22 <emamendez6@gmail.com>
2026-06-17 14:49:05 -0300

    act

---
+++ entrenamiento_03/ejercicio4/simonDice.js
@@ -0,0 +1,15 @@
+
+const red = document.createElement('div');
+const red = document.createElement('div');
+red.classList.add('card');
+function renderizarPaises(paises) {
+
+    const grilla = document.getElementById('grilla-paises');
+    grilla.innerHTML = '';
+    red.innerHTML = `
+        <div> 
+        </div>
+    `;
+
+    red.addEventListener('click', () => sumarSecuencia(numero));
+    grilla.appendChild(red);
